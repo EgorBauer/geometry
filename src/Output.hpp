468d195ae5851b8e8bca0d4efe8da7d6a5acee79
egorbauer <egorbauer@yandex.ru>
2020-06-22 19:07:12 +0700

    Added Output.hpp

---
+++ src/Output.hpp
@@ -0,0 +1,16 @@
+#ifndef H_OUTPUT
+#define H_OUTPUT
+
+#include "Figure.cpp"
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+void outputF(figure* circle, int k);
+int sqr(int a);
+#endif
+
+
